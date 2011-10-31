1bf3e08bdfa2da7b87af5a7c012f97d5251cdb0f
Nick Boudreau <ntboudreau@gmail.com>
2011-10-31 17:13:57 -0500

    exercises for chapter 8

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -45,8 +45,30 @@ describe UsersController do
       get 'new'
       response.should have_selector("title", :content => "sign up")
     end
-  end
+    
+    it "should have a name field" do
+      get :new
+      response.should have_selector("input[name='user[name]'] [type='text']")
+    end
   
+    it "should have an email field" do
+      get :new
+      response.should have_selector("input[name='user[email]'] [type='text']")
+    end
+    
+    it "should have a password field" do
+      get :new
+      response.should have_selector("input[name='user[password]'] 
+                                          [type='password']")
+    end
+    
+    it "should have a password confirmation field" do
+      get :new
+      response.should have_selector("input[name='user[password_confirmation]']
+                                          [type='password']")
+    end
+  end
+
   describe "POST 'create'" do
     
     describe "failure" do
